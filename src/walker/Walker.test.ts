6d8ec0a66a840fd1ebfdfc1f4537e1709f61c1d3
Thomas Haywood <thomashaywoodproductions@gmail.com>
2020-04-03 00:37:41 +0100

    Started on fixing Walker

--- src/walker/Walker.test.ts
+++ src/walker/Walker.test.ts
@@ -1,160 +1,162 @@
-// import Walker from './index';
-// import { DocumentConfig } from '../config/generic/MainConfig';
-// import { OnEventConfig } from '../config/generic/EventConfigs';
-// import { TreeStructure, TreeArrayElement } from './TreeStructure';
-// import { ArrayFieldConfig, ArrayConfig } from '../config/generic/FieldConfigs';
+import Walker from './Walker';
+import { DocumentConfig } from '../config/generic/MainConfig';
+import { OnEventConfig } from '../config/generic/EventConfigs';
+import { FieldConfig, ArrayConfig, ArrayFieldConfig } from '../config/generic/FieldConfigs';
+import { FieldId } from '../config/generic/ConditionsConfigs';
 
-// const extractLevelsFromWalker = (documentConfig: DocumentConfig, onEventConfig: OnEventConfig): {level: number, path: string[], type: TreeStructure}[] => {
-//   const output: {level: number, path: string[], type: TreeStructure}[] = [];
+const extractLevelsFromWalker = (documentConfig: DocumentConfig, onEventConfig: OnEventConfig): {fieldId: FieldId, type: FieldConfig}[] => {
+  const output: {fieldId: FieldId, type: FieldConfig}[] = [];
   
-//   Walker(documentConfig, onEventConfig, (level: number, path: string[], type: TreeStructure) => {
-//     output.push({
-//       level,
-//       path,
-//       type
-//     });
-//   });
+  Walker(documentConfig, onEventConfig, (fieldId: FieldId, type: FieldConfig) => {
+    output.push({
+      fieldId,
+      type
+    });
+  });
 
-//   return output;
-// }
+  return output;
+}
 
-// describe("Walker", () => {
-//   describe("onLevel", () => {
-//     const basicTypes: ("string"|"boolean"|"number"|"timestamp")[] = ["string", "boolean", "number", "timestamp"];
-//     basicTypes.forEach(type => {
-//       describe(type, () => {
-//         [true, false].forEach(required => {
-//           test(required ? "Required" : "Optional", () => {
-//             const documentConfig: DocumentConfig = {
-//               id: "docId",
-//               description: "abc",
-//               fields: {
-//                 "field": {
-//                   type: type,
-//                   required: required
-//                 }
-//               }
-//             };
+const basicTypes: ("string"|"boolean"|"number"|"timestamp")[] = ["string", "boolean", "number", "timestamp"];
+
+describe("Walker", () => {
+  describe("onLevel", () => {
+
+    basicTypes.forEach(type => {
+      describe(type, () => {
+        [true, false].forEach(required => {
+          test(required ? "Required" : "Optional", () => {
+            const documentConfig: DocumentConfig = {
+              id: "docId",
+              description: "abc",
+              fields: {
+                "field": {
+                  type: type,
+                  required: required
+                }
+              }
+            };
             
-//             const onEventConfig: OnEventConfig = {
-//               fields: ["field"]
-//             };
+            const onEventConfig: OnEventConfig = {
+              fields: ["field"]
+            };
 
-//             expect(extractLevelsFromWalker(documentConfig, onEventConfig)).toStrictEqual([
-//               {
-//                 level: 0,
-//                 path: [],
-//                 type: {
-//                   "field": {
-//                     type: type,
-//                     required: required,
-//                   }
-//                 }
-//               }
-//             ]);
-//           });
-//         });
-//       });
-//     });
+            expect(extractLevelsFromWalker(documentConfig, onEventConfig)).toStrictEqual([
+              {
+                level: 0,
+                path: [],
+                type: {
+                  "field": {
+                    type: type,
+                    required: required,
+                  }
+                }
+              }
+            ]);
+          });
+        });
+      });
+    });
     
-//     const arrayTypes: {
-//       type: "array", 
-//       format: ArrayFieldConfig,
-//       expected: TreeArrayElement
-//     }[] = [
-//       {
-//         type: "array", 
-//         format: {
-//           type: "string"
-//         },
-//         expected: {
-//           type: "string"
-//         }
-//       },
-//       {
-//         type: "array", 
-//         format: {
-//           type: "number"
-//         },
-//         expected: {
-//           type: "number"
-//         }
-//       },
-//       {
-//         type: "array", 
-//         format: {
-//           type: "boolean"
-//         },
-//         expected: {
-//           type: "boolean"
-//         }
-//       },
-//       {
-//         type: "array", 
-//         format: {
-//           type: "timestamp"
-//         },
-//         expected: {
-//           type: "timestamp"
-//         }
-//       },
-//       {
-//         type: "array", 
-//         format: {
-//           type: "object",
-//           fields: {}
-//         },
-//         expected: {
-//           type: "object",
-//           map: {}
-//         }
-//       }
-//     ];
+    // const arrayTypes: {
+    //   type: "array", 
+    //   format: ArrayFieldConfig,
+    //   expected: ArrayFieldConfig
+    // }[] = [
+    //   {
+    //     type: "array", 
+    //     format: {
+    //       type: "string"
+    //     },
+    //     expected: {
+    //       type: "string"
+    //     }
+    //   },
+    //   {
+    //     type: "array", 
+    //     format: {
+    //       type: "number"
+    //     },
+    //     expected: {
+    //       type: "number"
+    //     }
+    //   },
+    //   {
+    //     type: "array", 
+    //     format: {
+    //       type: "boolean"
+    //     },
+    //     expected: {
+    //       type: "boolean"
+    //     }
+    //   },
+    //   {
+    //     type: "array", 
+    //     format: {
+    //       type: "timestamp"
+    //     },
+    //     expected: {
+    //       type: "timestamp"
+    //     }
+    //   },
+    //   {
+    //     type: "array", 
+    //     format: {
+    //       type: "object",
+    //       fields: {}
+    //     },
+    //     expected: {
+    //       type: "object",
+    //       fields: {}
+    //     }
+    //   }
+    // ];
 
-//     arrayTypes.forEach(v => {
-//       describe(v.type + " " + v.format.type, () => {
-//         [true, false].forEach(required => {
-//           test(required ? "Required" : "Optional", () => {
-//             const fieldConfig: ArrayConfig = {
-//               type: "array",
-//               required: required,
-//               valueType: v.format
-//             };
+    // arrayTypes.forEach(v => {
+    //   describe(v.type + " " + v.format.type, () => {
+    //     [true, false].forEach(required => {
+    //       test(required ? "Required" : "Optional", () => {
+    //         const fieldConfig: ArrayConfig = {
+    //           type: "array",
+    //           required: required,
+    //           valueType: v.format
+    //         };
 
-//             const documentConfig: DocumentConfig = {
-//               id: "docId",
-//               description: "abc",
-//               fields: {
-//                 field: fieldConfig
-//               }
-//             };
+    //         const documentConfig: DocumentConfig = {
+    //           id: "docId",
+    //           description: "abc",
+    //           fields: {
+    //             field: fieldConfig
+    //           }
+    //         };
             
-//             const onEventConfig: OnEventConfig = {
-//               fields: ["field"]
-//             };
+    //         const onEventConfig: OnEventConfig = {
+    //           fields: ["field"]
+    //         };
 
-//             expect(extractLevelsFromWalker(documentConfig, onEventConfig)).toStrictEqual([
-//               {
-//                 level: 0,
-//                 path: [],
-//                 type: {
-//                   "field": v.type == "array" ? {
-//                     required,
-//                     type: "array",
-//                     valueType: v.expected
-//                   } : {
-//                     required,
-//                     type: "object",
-//                     valueType: v.expected
-//                   }
-//                 }
-//               }
-//             ]);
-//           });
-//         });
-//       });
-//     });
-//   });
+    //         expect(extractLevelsFromWalker(documentConfig, onEventConfig)).toStrictEqual([
+    //           {
+    //             level: 0,
+    //             path: [],
+    //             type: {
+    //               "field": v.type == "array" ? {
+    //                 required,
+    //                 type: "array",
+    //                 valueType: v.expected
+    //               } : {
+    //                 required,
+    //                 type: "object",
+    //                 valueType: v.expected
+    //               }
+    //             }
+    //           }
+    //         ]);
+    //       });
+    //     });
+    //   });
+    // });
+  });
+});
     
 // //   describe("onConfig", () => {
 // //     const tests: {
